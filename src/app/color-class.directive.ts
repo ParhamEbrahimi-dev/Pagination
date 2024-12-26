eb4384c7f0342d306524703298b38b3449d1bd5c
Parham Ebrahimi <ParhamEbrahimi.dev@gmail.com>
2024-12-26 20:42:03 +0330

    add HostListener and HostBinding

--- src/app/color-class.directive.ts
+++ src/app/color-class.directive.ts
@@ -1,15 +1,23 @@
-import { Directive,ElementRef } from '@angular/core';
+import { Directive,ElementRef, HostBinding, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appColorClass]'
 })
 export class ColorClassDirective {
+  @HostBinding("style.backgroundColor") backgroundColor : string="transparent"
 
   constructor(private elementRef : ElementRef ) 
   {
-    console.log(this.elementRef)
-    this.elementRef.nativeElement.style.backgroundColor='white';
+    //console.log(this.elementRef)
+   // this.elementRef.nativeElement.style.backgroundColor='white';
 
+  }
+  @HostListener("mouseenter") onMouseEnter(){
+    this.backgroundColor="Red";
+  }
+  @HostListener("mouseleave") onMouseLeave()
+  {
+    this.backgroundColor="yellow";
   }
 
 }
