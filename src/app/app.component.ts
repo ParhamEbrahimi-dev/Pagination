4fafb2bdd358b8c1f3b7f73d75d85096feb039d5
Parham Ebrahimi <ParhamEbrahimi.dev@gmail.com>
2024-12-26 19:43:56 +0330

    Add ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,9 @@ export class AppComponent {
   setActivePage(i : number) : string{
     return this.currentPageIndex==i ? "active":"";
   }
+  CheckindexVisibility(index : number):boolean{
+    return Math.abs(this.currentPageIndex-index)<5
+  }
   images=[
     {
       title : "At The Beach1",
@@ -21,15 +24,47 @@ export class AppComponent {
     },
     {
       title : "At The Beach2",
-      Url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvb4a18gBEfwbg5ojk6MI6JpzQbnb-OpUirA&s",  
+      Url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOeNxhT1OVBquTqDPet33UqbBtvrm7I9PQvw&s",  
     },
     {
       title : "At The Beach3",
-      Url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvb4a18gBEfwbg5ojk6MI6JpzQbnb-OpUirA&s",  
+      Url : "https://images.photowall.com/products/73393/perfect-beach.jpg?h=699&q=85",  
     },
     {
       title : "At The Beach4",
+      Url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeeDg3_B6u6R6ZFFqKD93WWMppOYal1Gj4vw&s",  
+    },
+    {
+      title : "At The Beach5",
       Url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvb4a18gBEfwbg5ojk6MI6JpzQbnb-OpUirA&s",  
+    },
+    {
+      title : "At The Beach6",
+      Url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOeNxhT1OVBquTqDPet33UqbBtvrm7I9PQvw&s",  
+    },
+    {
+      title : "At The Beach7",
+      Url : "https://images.photowall.com/products/73393/perfect-beach.jpg?h=699&q=85",  
+    },
+    {
+      title : "At The Beach8",
+      Url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeeDg3_B6u6R6ZFFqKD93WWMppOYal1Gj4vw&s",  
+    },
+    {
+      title : "At The Beach9",
+      Url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvb4a18gBEfwbg5ojk6MI6JpzQbnb-OpUirA&s",  
+    },
+    {
+      title : "At The Beach10",
+      Url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOeNxhT1OVBquTqDPet33UqbBtvrm7I9PQvw&s",  
+    },
+    {
+      title : "At The Beach11",
+      Url : "https://images.photowall.com/products/73393/perfect-beach.jpg?h=699&q=85",  
+    },
+    {
+      title : "At The Beach12",
+      Url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeeDg3_B6u6R6ZFFqKD93WWMppOYal1Gj4vw&s",  
     },
     
   ]
