cbc2f7bb9c5c69d1d1d8486bd6305435617d5dad
Parham Ebrahimi <ParhamEbrahimi.dev@gmail.com>
2024-12-26 18:10:16 +0330

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Pagination';
+  images=[
+    {
+      title : "At The Beach",
+      Url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvb4a18gBEfwbg5ojk6MI6JpzQbnb-OpUirA&s",  
+    },
+    {
+      title : "At The Beach",
+      Url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvb4a18gBEfwbg5ojk6MI6JpzQbnb-OpUirA&s",  
+    },
+    {
+      title : "At The Beach",
+      Url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvb4a18gBEfwbg5ojk6MI6JpzQbnb-OpUirA&s",  
+    },
+    {
+      title : "At The Beach",
+      Url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvb4a18gBEfwbg5ojk6MI6JpzQbnb-OpUirA&s",  
+    },
+    
+  ]
 }
